3108d4d7ab94c61ef161e619590160692e301040
Alexandre Levy <levy_a@etna-alternance.net>
2018-08-13 00:13:07 +0200

    #1 secure entry and readfile

---
+++ CarteAuTresor/CarteAuTresor/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CarteAuTresor
+{
+    class FileManager
+    {
+        private string pathToFile;
+        private string pathToOutFile;
+
+        public FileManager(string pathToFile)
+        {
+            //Check if the path send in param exist realy
+            if (File.Exists(pathToFile)) {
+                this.pathToFile = pathToFile;
+            } else {
+                throw new Exception("file doesn't exist");
+            }
+           
+        }
+
+
+        public void readFileMap()
+        {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(this.pathToFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+        }
+
+
+    }
+}
---
+++ CarteAuTresor/CarteAuTresor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace CarteAuTresor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) {
+                Console.WriteLine("You need to set a pathfile");
+            } else {
+                FileManager fileManager = new FileManager(args[0]);
+                fileManager.readFileMap();
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
